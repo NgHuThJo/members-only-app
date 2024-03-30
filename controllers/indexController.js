3903ffca9ec72914211cabdf1468f094de3c11e6
John Doe <nghuthjo@gmail.com>
2024-03-30 21:54:28 +0100

    Finished signup form validation

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -2,8 +2,6 @@ const User = require("../models/user");
 const { body, validationResult } = require("express-validator");
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcryptjs");
-const createInputTextCheck =
-  require("../express-validator/customValidationChains").createInputTextCheck;
 
 exports.index = asyncHandler(async (req, res, next) => {
   res.render("layout", {
@@ -20,10 +18,35 @@ exports.signUpGet = (req, res, next) => {
 
 exports.signUpPost = [
   // Form field validation and sanitization
-  createInputTextCheck("firstName"),
-  createInputTextCheck("lastName"),
-  createInputTextCheck("userName"),
-  createInputTextCheck("password"),
+  body("firstName")
+    .trim()
+    .isLength({ min: 1 })
+    .withMessage("First name must not be empty.")
+    .escape(),
+  body("lastName")
+    .trim()
+    .isLength({ min: 1 })
+    .withMessage("Last name must not be empty.")
+    .escape(),
+  // Check whether user name is already in database
+  body("userName")
+    .custom(async (value) => {
+      const user = await User.find({ user_name: value });
+
+      if (user.length > 0) {
+        return Promise.reject();
+      }
+    })
+    .withMessage("User name is already in use."),
+  body("password")
+    .trim()
+    .isLength({ min: 1 })
+    .withMessage("Password must not be empty.")
+    .escape(),
+  // Check whether password field values match
+  body("confirmPassword")
+    .custom((value, { req }) => value === req.body.password)
+    .withMessage("Passwords do not match."),
   // Business logic
   asyncHandler(async (req, res, next) => {
     const errors = validationResult(req);
@@ -35,26 +58,27 @@ exports.signUpPost = [
       password: req.body.password,
     });
 
-    if (!errors.isEmpty) {
+    if (!errors.isEmpty()) {
       res.render("layout", {
         title: "Signup page",
         user,
+        errors: errors.array(),
         content: "signup",
       });
+    } else {
+      // Create hashed password
+      bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+        if (err) {
+          next(err);
+        }
+
+        user.password = hashedPassword;
+
+        await user.save();
+      });
+
+      res.redirect("/");
     }
-
-    // Create hashed password
-    bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
-      if (err) {
-        next(err);
-      }
-
-      user.password = hashedPassword;
-
-      await user.save();
-    });
-
-    res.redirect("/");
   }),
 ];
 
